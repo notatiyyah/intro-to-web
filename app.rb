4f78a6d303c4e29f6d974683d2c017aae4c6391c
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-05 15:24:20 +0000

    Use variables instead of executing code in views

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ get '/' do
 end
 
 get '/cat' do
-    erb(:index)
+    erb(:index, :locals => {:name => ["Amigo", "Oscar", "Viking"].sample})
+    # Syntax: erb( :name_of_file(view), :locals => hash_of_variables_to_pass_to_template )
 end
 
 get '/secret' do

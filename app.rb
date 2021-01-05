9122d685adb953798cec5fb1d1e9055164f72528
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-05 15:32:37 +0000

    Use query strings in named-cat route

--- app.rb
+++ app.rb
@@ -4,10 +4,15 @@ get '/' do
     "hello!"
 end
 
-get '/cat' do
+get '/random-cat' do
     erb(:index, :locals => {:name => ["Amigo", "Oscar", "Viking"].sample})
     # Syntax: erb( :name_of_file(view), :locals => hash_of_variables_to_pass_to_template )
 end
+
+get '/named-cat' do
+    erb(:index, :locals => {:name => params[:name] })
+end
+
 
 get '/secret' do
     "Shhh it's a secret"

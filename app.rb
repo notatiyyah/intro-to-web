f60008d0bef9148b07101072b5bc2d65c6f3c2c4
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-07 14:26:28 +0000

    Set up shotgun config & use POST rrequests after form

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
-require 'sinatra'
-
-get '/' do
-    "hello!"
-end
-
-get '/random-cat' do
-    erb(:index, :locals => { :name => ["Amigo", "Oscar", "Viking"].sample, :form => false })
-    # Syntax: erb( :name_of_file(view), :locals => hash_of_variables_to_pass_to_template )
-end
-
-get '/named-cat' do
-    erb(:index, :locals => { :name => params[:name], :form => true })
-end
+require 'sinatra/base'
 
 
-get '/secret' do
-    "Shhh it's a secret"
+class CatApp < Sinatra::Base
+
+    get '/' do
+        "hello!"
+    end
+
+    get '/random-cat' do
+        erb(:index, :locals => { :name => ["Amigo", "Oscar", "Viking"].sample, :form => false })
+        # Syntax: erb( :name_of_file(view), :locals => hash_of_variables_to_pass_to_template )
+    end
+
+    get '/named-cat' do
+        erb(:index, :locals => { :name => params[:name], :form => true })
+    end
+
+    post '/named-cat' do
+        erb(:index, :locals => { :name => params[:name], :form => false })
+    end
+
+
+    get '/secret' do
+        "Shhh it's a secret"
+    end
+
+    run! if app_file == $0
 end
